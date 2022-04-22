44f420c8ed281e7dd60bac0df957838ea495b1d7
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-04-22 16:45:20 -0400

    use JSON with body

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post('/app/flip', (req, res) => {
 });
 
 app.post('/app/flips/:number', (req, res) => {
-    let arr = coinFlips(req.params.number);
+    let arr = coinFlips(req.body.number);
     res.status(200).json({ raw: arr, summary: countFlips(arr) });
 });
 
