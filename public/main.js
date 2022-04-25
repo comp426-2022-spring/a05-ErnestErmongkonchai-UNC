21cc132ebdead026697ec6a8af299e02ed3062ca
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-04-24 23:14:52 -0400

    singlecoin

--- public/main.js
+++ public/main.js
@@ -48,7 +48,15 @@ document.getElementById("guessnav").onclick = function(){
     document.getElementById("guess").className = "";
 };
 // Flip one coin and show coin image to match result when button clicked
-
+function single_coin() {
+    fetch('http://localhost:5555/app/flip/', {mode: 'cors'}).then(function(response) {
+        return response.json();
+    }).then(function(result) {
+        console.log(result);
+        document.getElementById("resultsingle").innerHTML = result.flip;
+        document.getElementById("quarter").src=`./assets/img/${result.flip}.png`
+    })
+}
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
