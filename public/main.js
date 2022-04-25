1f7cc9c457c1d531f4f03429b2e8e2f456d2dba7
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-04-24 23:08:25 -0400

    main change

--- public/main.js
+++ public/main.js
@@ -1,5 +1,52 @@
 // Focus div based on nav button click
+document.getElementById("homenav").onclick = function() {
+    document.location.reload();
+    document.getElementById("homenav").className = "active";
+    document.getElementById("singlenav").className = "";
+    document.getElementById("multinav").className = "";
+    document.getElementByIdii("guessnav").className = "";
 
+    document.getElementById("home").className = "";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "hidden";
+}
+
+document.getElementById("singlenav").onclick = function(){
+    document.getElementById("homenav").className = "";
+    document.getElementById("singlenav").className = "active";
+    document.getElementById("multinav").className = "";
+    document.getElementById("guessnav").className = "";
+
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "hidden";
+};
+
+document.getElementById("multinav").onclick = function(){
+    document.getElementById("homenav").className = "";
+    document.getElementById("singlenav").className = "";
+    document.getElementById("multinav").className = "active";
+    document.getElementById("guessnav").className = "";
+
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "";
+    document.getElementById("guess").className = "hidden";
+};
+
+document.getElementById("guessnav").onclick = function(){
+    document.getElementById("homenav").className = "";
+    document.getElementById("singlenav").className = "";
+    document.getElementById("multinav").className = "";
+    document.getElementById("guessnav").className = "active";
+
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "";
+};
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
